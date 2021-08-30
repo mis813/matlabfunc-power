ae398c8bedfd89476ed291283b236ba250817a39
Abdolmon'em islami dulabi <islamidulabi813@gmail.com>
2021-08-30 06:58:08 +0430

    Update disppol.m

--- disppol.m
+++ disppol.m
@@ -1,14 +1,13 @@
-function [num] = disppol(mata)
+function [num] = disppol(matA)
 %% input matrix
 %%
-num = string(zeros(size(mata)));
-
-[k, j] = size(mata); 
-for l = 1:k
-for i = 1:j
-    num(l,i) = string(num2str(abs(mata(l,i)))+"<"+num2str(rad2deg(angle(mata(l,i)))))+"°";
-end
-end
+num = string(zeros(size(matA)));
+[k, j] = size(matA);
+    for l = 1:k
+        for i = 1:j
+            num(l,i) = string(num2str(abs(mata(l,i)))+"<"+num2str(rad2deg(angle(mata(l,i)))))+"°";
+        end
+    end
 
 end
 
